6166fc83f28aac52d3652db4bc97cd1d5e37fa21
dallasreizevoort <dallasreizevoort@gmail.com>
2024-02-05 00:13:51 -0800

    TODO

--- dallas-reizevoort-capstone-app/src/App.js
+++ dallas-reizevoort-capstone-app/src/App.js
@@ -6,6 +6,21 @@ import "./styles/partials/global.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./Auth/AuthContext";
 
+/*TO DO:
+1. Store token in server
+2. Setup tablet and desktop responsive designs. Clean up mobile
+3. Fix "Stats" dropdown
+4. Fix Footer
+5. Refactor code. Clean up unused code
+6. Remove Console.logs
+7. Make HTML and SCSS more semantic
+8. Create database for user data. Store users song data to create
+a "Compare to last visit" feature that will show updated song rankings.
+9. Optimze code for performance
+10. Test for more bugs.
+11. fix create playlist description.
+*/
+
 function App() {
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get("code");
